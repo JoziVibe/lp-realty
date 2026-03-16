11513c246b2cc30c7c82a03ebdb96accacc4c9bf
Dani Raulisa <dani@betterforce.co>
2026-03-16 05:51:40 +0000

    Make Section full width (_for element section_)

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -34,22 +34,24 @@ export function TestimonialsSection({
   }));
 
   return (
-    <Section className={cn(
+    <Section removePadding className={cn(
       "bg-secondary text-foreground overflow-hidden",
       "py-12 sm:py-24 md:py-32",
       className
     )}>
-      <div className="flex flex-col items-center gap-4 text-center sm:gap-8 w-full">
-        <div className="flex flex-col items-center gap-4 px-4 sm:gap-8 max-w-container mx-auto">
-          <h2 className="max-w-[720px] text-4xl font-medium leading-tight sm:text-5xl sm:leading-tight">
-            {title}
-          </h2>
-          <p className="text-md max-w-[600px] text-muted-foreground sm:text-lg">
-            {description}
-          </p>
+      <div className="container">
+        <div className="flex flex-col items-center gap-4 text-center sm:gap-8 w-full">
+          <div className="flex flex-col items-center gap-4 px-4 sm:gap-8">
+            <h2 className="max-w-[720px] text-4xl font-medium leading-tight sm:text-5xl sm:leading-tight">
+              {title}
+            </h2>
+            <p className="text-md max-w-[600px] text-muted-foreground sm:text-lg">
+              {description}
+            </p>
+          </div>
+          
+          <TestimonialsMinimal testimonials={mappedTestimonials} />
         </div>
-        
-        <TestimonialsMinimal testimonials={mappedTestimonials} />
       </div>
     </Section>
   )
